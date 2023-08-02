3f665abb5d4b3bcbafb9a8939ce413862664aac7
Denis-Sklyarov123 <unrealgoodandyou@mail.ru>
2023-08-02 17:53:15 +0300

    Added pubsub component

---
+++ src/components/pubsub/moduleA.js
@@ -0,0 +1,15 @@
+import {arrMenuItems} from '/src/constants/index'
+
+const pubSub = require('./pubsub')
+
+module.exports = {
+  publishEvent() {
+    const data = arrMenuItems
+
+    pubSub.publish('Hey Jony', data)
+  },
+}
+
+// Здесь нам требуется модуль pubSub, и мы экспортируем объект с помощью метода publishEvent.
+// Метод указывает некоторые данные, а затем вызывает метод pubSub.publish,
+// передавая имя публикуемого события и созданный нами объект данных.
